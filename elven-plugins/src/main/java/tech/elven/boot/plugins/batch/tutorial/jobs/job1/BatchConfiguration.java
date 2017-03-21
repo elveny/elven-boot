a7516a426b757252dbbb7f0c0df211412af3a6c7
elveny <elve1984@163.com>
2017-03-22 01:04:33 +0800

    增加到两个Step处理，试用AbstractPaginatedDataItemReader,FlatFileItemWriter,FormatterLineAggregator,DelimitedLineAggregator

--- elven-plugins/src/main/java/tech/elven/boot/plugins/batch/tutorial/jobs/job1/BatchConfiguration.java
+++ elven-plugins/src/main/java/tech/elven/boot/plugins/batch/tutorial/jobs/job1/BatchConfiguration.java
@@ -13,21 +13,30 @@ import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.listener.JobExecutionListenerSupport;
-import org.springframework.batch.item.*;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.data.AbstractPaginatedDataItemReader;
 import org.springframework.batch.item.database.JpaItemWriter;
 import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.FlatFileItemWriter;
 import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
 import org.springframework.batch.item.file.mapping.DefaultLineMapper;
+import org.springframework.batch.item.file.transform.BeanWrapperFieldExtractor;
 import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
+import org.springframework.batch.item.file.transform.FormatterLineAggregator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.PathResource;
+import org.springframework.data.domain.PageRequest;
 import tech.elven.boot.plugins.data.jpa.entity.User;
 import tech.elven.boot.plugins.data.jpa.repository.UserRepository;
 
 import javax.persistence.EntityManagerFactory;
+import java.util.Iterator;
 import java.util.List;
 
 
@@ -100,24 +109,104 @@ public class BatchConfiguration {
         return items -> userRepository.save(items);
     }
 
+    /////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    @Bean
     public ItemReader<User> reader1() {
-        return new ItemReader<User>() {
+        AbstractPaginatedDataItemReader<User> reader = new AbstractPaginatedDataItemReader<User>() {
             @Override
-            public User read() throws Exception, UnexpectedInputException, ParseException, NonTransientResourceException {
-                return null;
+            protected Iterator<User> doPageRead() {
+                return userRepository.findAll(new PageRequest(page, pageSize)).getContent().iterator();
             }
         };
+        reader.setPageSize(2);
+        reader.setName("reader1");
+
+        /*ItemReader<User> reader = new AbstractPaginatedDataItemReader<User>(){
+            @Override
+            protected Iterator<User> doPageRead() {
+                return userRepository.findAll(new PageRequest(page, pageSize)).getContent().iterator();
+            }
+        };*/
+        return reader;
     }
+
+    public ItemProcessor<User, Person> processor1(){
+        return user -> {
+
+            long id = user.getId();
+            String name = user.getName();
+            String firstName = name;
+            String lastName = name;
+            if(name.contains(",")){
+                firstName = name.split(",")[0];
+                lastName = name.split(",")[1];
+            }
+
+            firstName = id + "." + firstName;
+            return new Person(firstName, lastName);
+        };
+    }
+
+    public ItemWriter<Person> writer1(){
+        FlatFileItemWriter<Person> writer = new FlatFileItemWriter<Person>();
+        writer.setEncoding("utf-8");
+        writer.setLineSeparator("\r\n");
+        writer.setResource(new PathResource("out/tech/elven/boot/plugins/batch/tutorial/jobs/job1/sample-data1.csv"));
+        writer.setAppendAllowed(true);
+        writer.setShouldDeleteIfExists(true);
+
+        FormatterLineAggregator<Person> formatterLineAggregator = new FormatterLineAggregator<Person>();
+        formatterLineAggregator.setFormat("%-20s%-20s");
+        String[] names = {"lastName", "firstName"};
+        BeanWrapperFieldExtractor<Person> beanWrapperFieldExtractor = new BeanWrapperFieldExtractor<Person>();
+        beanWrapperFieldExtractor.setNames(names);
+        formatterLineAggregator.setFieldExtractor(beanWrapperFieldExtractor);
+        writer.setLineAggregator(formatterLineAggregator);
+
+        /*DelimitedLineAggregator<Person> delimitedLineAggregator = new DelimitedLineAggregator<Person>();
+        delimitedLineAggregator.setDelimiter("|");
+        String[] names = {"firstName","lastName"};
+        BeanWrapperFieldExtractor<Person> beanWrapperFieldExtractor = new BeanWrapperFieldExtractor<Person>();
+        beanWrapperFieldExtractor.setNames(names);
+        delimitedLineAggregator.setFieldExtractor(beanWrapperFieldExtractor);
+        writer.setLineAggregator(delimitedLineAggregator);*/
+
+        return writer;
+    }
+
+//    public ItemWriter<Person> writer1() {
+//        FlatFileItemWriter<Person> writer = new FlatFileItemWriter<Person>();
+//        writer.setEncoding("utf-8");
+//        writer.setResource(new PathResource("tech/elven/boot/plugins/batch/tutorial/jobs/job1/sample-data1.csv"));
+//        DelimitedLineAggregator<Person> delimitedLineAggregator = new DelimitedLineAggregator<Person>();
+//        delimitedLineAggregator.setDelimiter("|");
+//        String[] names = {"firstName","lastName"};
+//        BeanWrapperFieldExtractor<Person> beanWrapperFieldExtractor = new BeanWrapperFieldExtractor<Person>();
+//        beanWrapperFieldExtractor.setNames(names);
+//        delimitedLineAggregator.setFieldExtractor(beanWrapperFieldExtractor);
+//        writer.setLineAggregator(delimitedLineAggregator);
+//        return writer;
+//    }
     // end::readerwriterprocessor[]
 
     // tag::step[]
     @Bean
-    public Step step1() {
-        return stepBuilderFactory.get("step1")
+    public Step step() {
+        return stepBuilderFactory.get("step")
                 .<Person, User> chunk(2)
                 .reader(reader())
                 .processor(processor())
                 .writer(writer())
+                .build();
+    }
+
+    public Step step1(){
+        return stepBuilderFactory.get("step1")
+                .<User, Person> chunk(2)
+                .reader(reader1())
+                .processor(processor1())
+                .writer(writer1())
                 .build();
     }
 
@@ -129,7 +218,8 @@ public class BatchConfiguration {
         return jobBuilderFactory.get("importUserJob")
                 .incrementer(new RunIdIncrementer())
                 .listener(listener())
-                .flow(step1())
+                .flow(step())
+                .next(step1())
                 .end()
                 .build();
     }
