bd7e04f78cbcc706aaf1960cf71c8746baf505d1
qiusheng.wu <qiusheng.wu@msxf.com>
2017-03-22 19:58:36 +0800

    spring-batch tutorial job1 各种listener

--- elven-plugins/src/main/java/tech/elven/boot/plugins/batch/tutorial/jobs/job1/BatchConfiguration.java
+++ elven-plugins/src/main/java/tech/elven/boot/plugins/batch/tutorial/jobs/job1/BatchConfiguration.java
@@ -13,6 +13,7 @@ import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.listener.JobExecutionListenerSupport;
+import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
@@ -73,6 +74,7 @@ public class BatchConfiguration {
     // tag::readerwriterprocessor[]
     @Bean
     public ItemReader<Person> reader() {
+        logger.info("reader::start::");
         FlatFileItemReader<Person> reader = new FlatFileItemReader<Person>();
         reader.setResource(new ClassPathResource("tech/elven/boot/plugins/batch/tutorial/jobs/job1/sample-data.csv"));
         reader.setLineMapper(new DefaultLineMapper<Person>() {{
@@ -83,12 +85,15 @@ public class BatchConfiguration {
                 setTargetType(Person.class);
             }});
         }});
+        logger.info("reader::end::");
         return reader;
     }
 
     @Bean
     public ItemProcessor<Person, User> processor() {
+        logger.info("processor::start::");
         return person -> {
+            logger.info("processor::doing::");
             String firstName = person.getFirstName().toUpperCase();
             String lastName = person.getLastName().toUpperCase();
 
@@ -98,14 +103,16 @@ public class BatchConfiguration {
 
     @Bean
     public JpaItemWriter<User> jpaWriter(){
+        logger.info("jpaWriter::start::");
         JpaItemWriter<User> writer = new JpaItemWriter<User>();
         writer.setEntityManagerFactory(entityManagerFactory);
-
+        logger.info("jpaWriter::end::");
         return writer;
     }
 
     @Bean
     public ItemWriter<User> writer(){
+        logger.info("writer::start::");
         return items -> userRepository.save(items);
     }
 
@@ -113,13 +120,15 @@ public class BatchConfiguration {
 
     @Bean
     public ItemReader<User> reader1() {
+        logger.info("reader1::start::");
         AbstractPaginatedDataItemReader<User> reader = new AbstractPaginatedDataItemReader<User>() {
             @Override
             protected Iterator<User> doPageRead() {
+                logger.info("reader1::doing::");
                 return userRepository.findAll(new PageRequest(page, pageSize)).getContent().iterator();
             }
         };
-        reader.setPageSize(2);
+        reader.setPageSize(10);
         reader.setName("reader1");
 
         /*ItemReader<User> reader = new AbstractPaginatedDataItemReader<User>(){
@@ -128,11 +137,14 @@ public class BatchConfiguration {
                 return userRepository.findAll(new PageRequest(page, pageSize)).getContent().iterator();
             }
         };*/
+        logger.info("reader1::end::");
         return reader;
     }
 
     public ItemProcessor<User, Person> processor1(){
+        logger.info("processor1::start::");
         return user -> {
+            logger.info("processor1::doing::");
 
             long id = user.getId();
             String name = user.getName();
@@ -149,6 +161,8 @@ public class BatchConfiguration {
     }
 
     public ItemWriter<Person> writer1(){
+        logger.info("writer1::start::");
+
         FlatFileItemWriter<Person> writer = new FlatFileItemWriter<Person>();
         writer.setEncoding("utf-8");
         writer.setLineSeparator("\r\n");
@@ -171,6 +185,7 @@ public class BatchConfiguration {
         beanWrapperFieldExtractor.setNames(names);
         delimitedLineAggregator.setFieldExtractor(beanWrapperFieldExtractor);
         writer.setLineAggregator(delimitedLineAggregator);*/
+        logger.info("writer1::end::");
 
         return writer;
     }
@@ -194,6 +209,11 @@ public class BatchConfiguration {
     @Bean
     public Step step() {
         return stepBuilderFactory.get("step")
+                .listener(stepExecutionListener())
+                .listener(itemReadListener())
+                .listener(itemProcessListener())
+                .listener(itemWriteListener())
+                .listener(chunkListener())
                 .<Person, User> chunk(2)
                 .reader(reader())
                 .processor(processor())
@@ -203,6 +223,11 @@ public class BatchConfiguration {
 
     public Step step1(){
         return stepBuilderFactory.get("step1")
+                .listener(stepExecutionListener())
+                .listener(itemReadListener())
+                .listener(itemProcessListener())
+                .listener(itemWriteListener())
+                .listener(chunkListener())
                 .<User, Person> chunk(2)
                 .reader(reader1())
                 .processor(processor1())
@@ -217,7 +242,7 @@ public class BatchConfiguration {
     public Job importUserJob() {
         return jobBuilderFactory.get("importUserJob")
                 .incrementer(new RunIdIncrementer())
-                .listener(listener())
+                .listener(jobExecutionListener())
                 .flow(step())
                 .next(step1())
                 .end()
@@ -225,34 +250,120 @@ public class BatchConfiguration {
     }
     // end::job[]
 
-    // tag::joblistener[]
+    // tag::jobExecutionListener[]
     @Bean
-    public JobExecutionListener listener(){
+    public JobExecutionListener jobExecutionListener(){
         return new JobExecutionListenerSupport(){
             @Override
             public void beforeJob(JobExecution jobExecution) {
                 super.beforeJob(jobExecution);
-                logger.info("Id::"+jobExecution.getId());
-                logger.info("JobId::"+jobExecution.getJobId()+", CreateTime::"+jobExecution.getCreateTime()+", EndTime::"+jobExecution.getEndTime());
-                logger.info("JobId::"+jobExecution.getJobId()+", StartTime::"+jobExecution.getStartTime()+", EndTime::"+jobExecution.getEndTime());
+                logger.info("Listener::JobName::"+jobExecution.getJobInstance().getJobName()+"::before");
             }
 
             @Override
             public void afterJob(JobExecution jobExecution) {
                 super.afterJob(jobExecution);
-
-                if (jobExecution.getStatus() == BatchStatus.COMPLETED){
-                    logger.info("!!! JOB FINISHED! Time to verify the results");
-
-                    List<User> users = userRepository.findAll();
-
-                    for (User user : users){
-                        logger.info("Found <" + JSON.toJSONString(user) + "> in the database.");
-                    }
-
-                }
+                logger.info("Listener::JobName::"+jobExecution.getJobInstance().getJobName()+"::after");
             }
         };
     }
-    // end::joblistener[]
+
+    @Bean
+    public StepExecutionListener stepExecutionListener(){
+        return new StepExecutionListener(){
+
+            @Override
+            public void beforeStep(StepExecution stepExecution) {
+                logger.info("Listener::StepName::"+stepExecution.getStepName()+"::before");
+            }
+
+            @Override
+            public ExitStatus afterStep(StepExecution stepExecution) {
+                logger.info("Listener::StepName::"+stepExecution.getStepName()+"::after");
+                return stepExecution.getExitStatus();
+            }
+        };
+    }
+
+    @Bean
+    public ItemReadListener itemReadListener(){
+        return new ItemReadListener(){
+
+            @Override
+            public void beforeRead() {
+                logger.info("Listener::ItemRead::::before");
+            }
+
+            @Override
+            public void afterRead(Object item) {
+                logger.info("Listener::ItemRead::::after");
+            }
+
+            @Override
+            public void onReadError(Exception ex) {
+                logger.info("Listener::ItemRead::::ReadError");
+            }
+        };
+    }
+
+    @Bean
+    public ItemProcessListener itemProcessListener(){
+        return new ItemProcessListener(){
+            @Override
+            public void beforeProcess(Object item) {
+                logger.info("Listener::ItemProcess::"+JSON.toJSONString(item)+"::before");
+            }
+
+            @Override
+            public void afterProcess(Object item, Object result) {
+                logger.info("Listener::ItemProcess::"+JSON.toJSONString(item)+"::after");
+            }
+
+            @Override
+            public void onProcessError(Object item, Exception e) {
+                logger.info("Listener::ItemProcess::"+JSON.toJSONString(item)+"::ProcessError");
+            }
+        };
+    }
+
+    @Bean
+    public ItemWriteListener itemWriteListener(){
+        return new ItemWriteListener(){
+            @Override
+            public void beforeWrite(List items) {
+                logger.info("Listener::ItemWrite::"+JSON.toJSONString(items)+"::before");
+            }
+
+            @Override
+            public void afterWrite(List items) {
+                logger.info("Listener::ItemWrite::"+JSON.toJSONString(items)+"::after");
+            }
+
+            @Override
+            public void onWriteError(Exception exception, List items) {
+                logger.info("Listener::ItemWrite::"+JSON.toJSONString(items)+"::WriteError");
+            }
+        };
+    }
+
+    @Bean
+    public ChunkListener chunkListener(){
+        return new ChunkListener(){
+            @Override
+            public void beforeChunk(ChunkContext context) {
+                logger.info("Listener::Chunk::"+context+"::before");
+            }
+
+            @Override
+            public void afterChunk(ChunkContext context) {
+                logger.info("Listener::Chunk::"+context+"::after");
+            }
+
+            @Override
+            public void afterChunkError(ChunkContext context) {
+                logger.info("Listener::Chunk::"+context+"::ChunkError");
+            }
+        };
+    }
+    // end::jobExecutionListener[]
 }
