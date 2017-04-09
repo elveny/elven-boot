b0efc050b8ffe3b13a5d72029c79ccb4c8768d43
elveny <elve1984@163.com>
2017-04-09 23:50:37 +0800

    webmagic 注解模式

--- elven-web/src/main/java/tech/elven/boot/web/rest/test/WebMagicController.java
+++ elven-web/src/main/java/tech/elven/boot/web/rest/test/WebMagicController.java
@@ -9,8 +9,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import tech.elven.boot.common.enums.ResultStatus;
+import tech.elven.boot.plugins.webmagic.quickstart.baidu.xueshu.BaiduXueshuArticle;
 import tech.elven.boot.plugins.webmagic.quickstart.baidu.xueshu.BaiduXueshuProcessor;
+import us.codecraft.webmagic.Site;
 import us.codecraft.webmagic.Spider;
+import us.codecraft.webmagic.model.ConsolePageModelPipeline;
+import us.codecraft.webmagic.model.OOSpider;
 import us.codecraft.webmagic.pipeline.ConsolePipeline;
 import us.codecraft.webmagic.processor.example.GithubRepoPageProcessor;
 
@@ -55,7 +59,17 @@ public class WebMagicController {
         Spider.create(new BaiduXueshuProcessor())
                 .addUrl("http://xueshu.baidu.com/s?wd=spark&rsv_bp=0&tn=SE_baiduxueshu_c1gjeupa&rsv_spt=3&ie=utf-8&f=8&rsv_sug2=0&sc_f_para=sc_tasktype%3D%7BfirstSimpleSearch%7D")
                 .addPipeline(new ConsolePipeline())
-                .thread(5).start();
+                .thread(5)
+                .start();
+        return ResultStatus.SUCCESS.code();
+    }
+
+    @RequestMapping("baiduXueshuArticle")
+    public String baiduXueshuArticle(){
+        OOSpider.create(Site.me().setSleepTime(1000), new ConsolePageModelPipeline(), BaiduXueshuArticle.class)
+                .addUrl("http://xueshu.baidu.com/s?wd=paperuri%3A%28861e4d0498bd9b65a673e1d41f00bd34%29&filter=sc_long_sign&sc_ks_para=q%3DSpark%20Plasma%20Sintering%20of%20Alumina&sc_us=543466628458669838&tn=SE_baiduxueshu_c1gjeupa&ie=utf-8")
+                .thread(5)
+                .run();
         return ResultStatus.SUCCESS.code();
     }
 }
