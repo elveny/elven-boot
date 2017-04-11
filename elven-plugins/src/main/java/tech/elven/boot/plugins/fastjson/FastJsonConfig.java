5e0d056581b44a18502d9ee185beffc942356fcf
qiusheng.wu <qiusheng.wu@msxf.com>
2017-04-11 14:08:27 +0800

    FastJsonConfig配置 all*.exclude group: 'org.slf4j', module: 'slf4j-log4j12'

---
+++ elven-plugins/src/main/java/tech/elven/boot/plugins/fastjson/FastJsonConfig.java
@@ -0,0 +1,26 @@
+package tech.elven.boot.plugins.fastjson;
+
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
+import org.springframework.boot.autoconfigure.web.HttpMessageConverters;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+
+/**
+ * @author qiusheng.wu
+ * @Created 2017/1/6.
+ */
+@Configuration
+public class FastJsonConfig {
+
+    @Bean
+    public HttpMessageConverters fastJsonHttpMessageConverters() {
+        FastJsonHttpMessageConverter fastConverter = new FastJsonHttpMessageConverter();
+        com.alibaba.fastjson.support.config.FastJsonConfig fastJsonConfig = new com.alibaba.fastjson.support.config.FastJsonConfig();
+        fastJsonConfig.setSerializerFeatures(SerializerFeature.PrettyFormat);
+        fastConverter.setFastJsonConfig(fastJsonConfig);
+        HttpMessageConverter<?> converter = fastConverter;
+        return new HttpMessageConverters(converter);
+    }
+}
